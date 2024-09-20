5dac8e89ba0643272e6e721cca832e88cfe345f3
parkysparky <sp474@byu.edu>
2024-09-19 23:14:43 -0600

    passing pawnPromotionWhite()

--- shared/src/main/java/chess/PawnMovesCalculator.java
+++ shared/src/main/java/chess/PawnMovesCalculator.java
@@ -22,8 +22,12 @@ public class PawnMovesCalculator implements PieceMovesCalculator {
                 }
             }
             ChessPosition endPosition = new ChessPosition(j+1, i);
-            if(null == board.getPiece(endPosition)){ //normal move
-                moves.add(new ChessMove(position, endPosition, null));
+            if(null == board.getPiece(endPosition)){
+                if(8 == endPosition.getRow()){ //promotion move
+                    addPromotionMoves(position, endPosition, moves);
+                } else {
+                    moves.add(new ChessMove(position, endPosition, null));
+                }
             }
         }
         if(ChessGame.TeamColor.BLACK == piece.getTeamColor()){
